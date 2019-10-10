3c39708f56fd7dde2650b5c0c9392342142a3afa
Piotr Ociepka <pkociepka@gmail.com>
2019-10-10 19:47:09 +0200

    Skeleton

---
+++ src/main/java/pl/kpr/eesti_kaeparast/EestiKaeparastApplication.java
@@ -0,0 +1,13 @@
+package pl.kpr.eesti_kaeparast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EestiKaeparastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EestiKaeparastApplication.class, args);
+	}
+
+}
